e93ba1bea8ce2941efde7e7c7e7444204e0d3489
vlineros <victorlineros@gmail.com>
2019-06-14 18:40:07 -0500

    found typo in connection file, testing again

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 mysql = require("mysql");
 var connection;
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.jAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    port: 3306,
+    port: process.env.PORT || 3306,
     password: "password",
     database: "burgers_db"
   });
